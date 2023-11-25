214e1a807ae46c0d8bf320771c03f0dfac86cfc2
ciberjbr <ciberjbr@gmail.com>
2023-11-25 19:24:42 +0100

    Creo routing inicial

--- AngularVideojuegos/src/app/app-routing.module.ts
+++ AngularVideojuegos/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import  { ListaVideojuegosComponent }  from './lista-videojuegos/lista-videojuegos.component';
+import { DetallesVideojuegoComponent } from './detalles-videojuego/detalles-videojuego.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {
+    path : '', //En este caso decimos el componente que mostraremos por defecto
+    component : LoginComponent
+  },
+  {
+    path : 'listaVideojuegos', //cuando definimos el 'path' no puede empezar por '/'
+    component : ListaVideojuegosComponent
+  },
+  {
+    path : 'detallesVideojuego', //cuando definimos el 'path' no puede empezar por '/'
+    component : DetallesVideojuegoComponent
+  },
+  {
+    path : 'contacto', //cuando definimos el 'path' no puede empezar por '/'
+    component : ContactoComponent
+  },
+  {
+    path : 'sobreNosotros', //cuando definimos el 'path' no puede empezar por '/'
+    component : SobreNosotrosComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
